1c801ca11d66585dfd57d9f6175feec727214f86
AigulParamonova <aigul.paramonova@yandex.ru>
2022-09-20 16:03:19 +0300

    step 4.3 done

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,11 +1,21 @@
-import pytest
+#  import pytest
 
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
-@pytest.mark.parametrize('language', ["en-gb"])
-def test_guest_can_go_to_login_page(browser, language):
-    link = f"http://selenium1py.pythonanywhere.com/{language}/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+#  @pytest.mark.parametrize('language', ["en-gb"])
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
